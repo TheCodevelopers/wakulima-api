572ad4a7e1d08b1a5500466d5790ca7c3367750e
Sam Indiazi <saindiazi@gmail.com>
2019-04-08 19:38:15 -0500

    Initializing project

---
+++ src/main/java/org/wakulima/wakulimaapi/WakulimaApiApplication.java
@@ -0,0 +1,13 @@
+package org.wakulima.wakulimaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WakulimaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WakulimaApiApplication.class, args);
+	}
+
+}
